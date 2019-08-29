7c3465711b31a6b2aab5858b610c8ebba7aa84a8
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-08-30 00:33:35 +0900

    Add files via upload

---
+++ lib/httpclient/application.ex
@@ -0,0 +1,36 @@
+defmodule Httpclient.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  alias Httpclient.Endpoint
+
+  def start(_type, _args) do
+    """
+    children = [
+      # Starts a worker by calling: Httpclient.Worker.start_link(arg)
+      # {Httpclient.Worker, arg}
+    ]
+    """
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    #opts = [strategy: :one_for_one, name: Httpclient.Supervisor]
+    Supervisor.start_link(children(), opts())
+  end
+
+  defp children do
+    [
+      Endpoint
+    ]
+  end
+
+  defp opts do
+    [
+      strategy: :one_for_one,
+      name: Httpclient.Supervisor
+    ]
+  end
+end
